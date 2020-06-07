ccb4d96da8883eec7e7c054fa15cba7354117fc6
Bonnie Simon <bonniesimon@protonmail.com>
2020-06-08 01:39:54 +0530

    found out why re-render happens

--- client/src/components/PostPage/PostPage.js
+++ client/src/components/PostPage/PostPage.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PostPage = ({location, match}) => {
+    const [postDetails, setPostDetails] = useState({}); 
     console.log(match.params);
+    console.log(location, "Hey");
+    // if(location.state !== undefined){
+    //     setPostDetails(location.state);    
+    // }
     
     return (
         <div>
-            <h1>postPage</h1>
+            
         </div>
     )
 }
 
 export default PostPage
+
+
+
+/**
+ * !Rerender happens because of the <React.strictMode>
+ * It happens only in production.
+ * https://stackoverflow.com/questions/61328285/react-component-rendering-twice-when-using-usestate-hook
+ */
