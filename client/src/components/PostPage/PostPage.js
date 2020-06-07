2cdde6c20df79f3269de1adb00f909670f06d6e9
Bonnie Simon <bonniesimon@protonmail.com>
2020-06-08 01:46:39 +0530

    Made postPage work when state passed through Link

--- client/src/components/PostPage/PostPage.js
+++ client/src/components/PostPage/PostPage.js
@@ -1,16 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const PostPage = ({location, match}) => {
     const [postDetails, setPostDetails] = useState({}); 
     console.log(match.params);
-    console.log(location, "Hey");
-    // if(location.state !== undefined){
-    //     setPostDetails(location.state);    
-    // }
+    console.log(location);
     
+    useEffect(() => {
+        if(location.state !== undefined){
+            setPostDetails(location.state)
+        }else{
+            fetchPostDetails(match.params.postId);
+        }
+    })
+
+    const fetchPostDetails = (postId) => {
+        
+    }
+
     return (
         <div>
-            
+            <h1>{postDetails.title}</h1> 
+            <p>{postDetails.body} </p>
         </div>
     )
 }
